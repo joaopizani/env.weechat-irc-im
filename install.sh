244e4ba56bd0b4edfecc61c0a2735b57f33a700f
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-11-28 23:20:00 +0100

    initial install mklinks script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+REL_SRC=${BASH_SOURCE[0]}
+CANONICAL_SRC=$(readlink -f $REL_SRC)
+DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
+
+mkdir -p "${HOME}/bin"
+
+ln -s -f "${DIR}/inotify-daemon/inotify-daemon" "${HOME}/bin/inotify-daemon"
+hash -r
+
+# GNOME-based
+AUTOSTART_DIR="${HOME}/.config/autostart"
+mkdir -p ${AUTOSTART_DIR}
+cp "${DIR}/inotify-daemon.desktop" "${AUTOSTART_DIR}/inotify-daemon.desktop"
+
