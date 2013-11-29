167d9b9f6d6a9e6db80c08450f114668b338d95d
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-11-29 10:47:51 +0100

    install script now making links to config files

--- install.sh
+++ install.sh
@@ -4,8 +4,16 @@ REL_SRC=${BASH_SOURCE[0]}
 CANONICAL_SRC=$(readlink -f $REL_SRC)
 DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
-mkdir -p "${HOME}/bin"
 
+WEECHAT_HOME="${HOME}/.weechat"
+CONFFILES=('alias.conf' 'aspell.conf' 'buffers.conf' 'irc.conf' 'logger.conf' 'plugins.conf' 'weechat.conf')
+
+for conffile in "${CONFFILES[@]}"; do
+    ln -s -f "${DIR}/${conffile}" "${WEECHAT_HOME}/${conffile}"
+done
+
+
+mkdir -p "${HOME}/bin"
 ln -s -f "${DIR}/inotify-daemon/inotify-daemon" "${HOME}/bin/inotify-daemon"
 hash -r
 
