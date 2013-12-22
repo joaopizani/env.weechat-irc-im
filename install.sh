59b4c78144023d7d55a25d03778bb042569dbd5c
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-12-22 13:59:10 -0200

    added install script covering GNOME-autostart etc

--- install.sh
+++ install.sh
@@ -6,19 +6,12 @@ DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
 
 WEECHAT_HOME="${HOME}/.weechat"
+CONFDIR="${DIR}/conf"
 CONFFILES=('alias.conf' 'aspell.conf' 'buffers.conf' 'irc.conf' 'logger.conf' 'plugins.conf' 'weechat.conf')
 
+mkdir -p ${WEECHAT_HOME}
+
 for conffile in "${CONFFILES[@]}"; do
-    ln -s -f "${DIR}/${conffile}" "${WEECHAT_HOME}/${conffile}"
+    ln -s -f "${CONFDIR}/${conffile}" "${WEECHAT_HOME}/${conffile}"
 done
-
-
-mkdir -p "${HOME}/bin"
-ln -s -f "${DIR}/inotify-daemon/inotify-daemon" "${HOME}/bin/inotify-daemon"
-hash -r
-
-# GNOME-based
-AUTOSTART_DIR="${HOME}/.config/autostart"
-mkdir -p ${AUTOSTART_DIR}
-cp "${DIR}/inotify-daemon.desktop" "${AUTOSTART_DIR}/inotify-daemon.desktop"
 
