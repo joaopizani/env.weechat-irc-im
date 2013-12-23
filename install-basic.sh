d82dc4adace656785b7428e4ad8b5e7937aa3c5d
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-12-23 21:17:36 -0200

    refactorings suggested by ShellChecker, quotings, etc.

--- install-basic.sh
+++ install-basic.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-REL_SRC=${BASH_SOURCE[0]}
-CANONICAL_SRC=$(readlink -f $REL_SRC)
+REL_SRC="${BASH_SOURCE[0]}"
+CANONICAL_SRC=$(readlink -f "$REL_SRC")
 DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
 
@@ -9,7 +9,7 @@ WEECHAT_HOME="${HOME}/.weechat"
 CONFDIR="${DIR}/conf"
 CONFFILES=('alias.conf' 'aspell.conf' 'buffers.conf' 'irc.conf' 'logger.conf' 'plugins.conf' 'weechat.conf')
 
-mkdir -p ${WEECHAT_HOME}
+mkdir -p "${WEECHAT_HOME}"
 
 for conffile in "${CONFFILES[@]}"; do
     ln -s -f "${CONFDIR}/${conffile}" "${WEECHAT_HOME}/${conffile}"
--- install-with-inotify.sh
+++ install-with-inotify.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-REL_SRC=${BASH_SOURCE[0]}
-CANONICAL_SRC=$(readlink -f $REL_SRC)
+REL_SRC="${BASH_SOURCE[0]}"
+CANONICAL_SRC=$(readlink -f "$REL_SRC")
 DIR="$(cd -P "$(dirname $CANONICAL_SRC)" && pwd)"
 
-./install-basic.sh
+"${DIR}/install-basic.sh"
 
 mkdir -p "${HOME}/bin"
 ln -s -f "${DIR}/inotify-daemon/inotify-daemon" "${HOME}/bin/inotify-daemon"
@@ -12,6 +12,8 @@ hash -r
 
 # GNOME-based
 AUTOSTART_DIR="${HOME}/.config/autostart"
-mkdir -p ${AUTOSTART_DIR}
-cp "${DIR}/inotify-daemon.desktop" "${AUTOSTART_DIR}/inotify-daemon.desktop"
+mkdir -p "${AUTOSTART_DIR}"
+cp "${DIR}/inotify-daemon.desktop"  "${AUTOSTART_DIR}/inotify-daemon.desktop"
+
+start-stop-daemon --exec "${HOME}/bin/inotify-daemon" --start
 
