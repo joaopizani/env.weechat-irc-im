f5a6928332f26518eeb33366948ad2bcdff2747e
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-07-19 14:01:30 +0200

    bitlbee.conf added and installation modified

--- install-basic.sh
+++ install-basic.sh
@@ -2,20 +2,23 @@
 
 DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 
-
-sudo apt-get install weechat bitlbee{,-{dev,plugin-otr}}
-
-
-"${DIR}/conf/ircconf-generate.sh"
-
+BITLBEE_PREFIX_DEFAULT="/"
+BITLBEE_PREFIX="${1:-"${BITLBEE_PREFIX_DEFAULT}"}"
+BITLBEE_ETCDIR="${BITLBEE_PREFIX}/etc/bitlbee"
 
 WEECHAT_HOME="${HOME}/.weechat"
 CONFDIR="${DIR}/conf"
 CONFFILES=('alias.conf' 'aspell.conf' 'buffers.conf' 'irc.conf' 'logger.conf' 'plugins.conf' 'weechat.conf')
 
-mkdir -p "${WEECHAT_HOME}"
 
+sudo apt-get install weechat
+
+"${DIR}/conf/ircconf-generate.sh"
+
+mkdir -p "${WEECHAT_HOME}"
 for conffile in "${CONFFILES[@]}"; do
     ln -s -f -n "${CONFDIR}/${conffile}" "${WEECHAT_HOME}/${conffile}"
 done
 
+LNCMD="ln -s -f -n ${DIR}/bitlbee.conf ${BITLBEE_ETCDIR}/bitlbee.conf"
+${LNCMD} || sudo ${LNCMD}
