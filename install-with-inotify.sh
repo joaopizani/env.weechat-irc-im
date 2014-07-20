1ca27c9fdb39845b482dee17b2b70034637e9799
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-07-20 21:01:37 +0200

    making install-with-inotify actually setup the submodule

--- install-with-inotify.sh
+++ install-with-inotify.sh
@@ -4,6 +4,8 @@ DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 
 "${DIR}/install-basic.sh"
 
+git submodule update --init --recursive --remote
+git submodule foreach -q --recursive 'branch="$(git config -f $toplevel/.gitmodules submodule.$name.branch)"; git checkout $branch'
 mkdir -p "${HOME}/bin"
 ln -s -f -n "${DIR}/inotify-daemon/inotify-daemon" "${HOME}/bin/inotify-daemon"
 hash -r
