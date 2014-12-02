9af7e8d58c86a38eff66efe07f728f94511624a5
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-12-02 14:07:51 +0100

    moved bitlbee in install-basic to the bottom

--- install-basic.sh
+++ install-basic.sh
@@ -2,15 +2,6 @@
 
 DIR="$(cd -P "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")" )" && pwd)"
 
-BITLBEE_PREFIX_DEFAULT="${HOME}/build/bitlbee"
-BITLBEE_PREFIX="$(readlink -m "${1:-"${BITLBEE_PREFIX_DEFAULT}"}" )"
-
-BITLBEE_SRC_VER_DEFAULT="3.2.2"
-BITLBEE_SRC_VER="${2:-"${BITLBEE_SRC_VER_DEFAULT}"}"
-
-BITLBEE_BUILDDIR_ROOT_DEFAULT="/tmp"
-BITLBEE_BUILDDIR_ROOT="$(readlink -m "${3:-"${BITLBEE_BUILDDIR_ROOT_DEFAULT}"}" )"
-
 
 WEECHAT_HOME="${HOME}/.weechat"
 CONFDIR="${DIR}/conf"
@@ -21,6 +12,16 @@ CONFFILES=('alias' 'aspell' 'buffers' 'irc' 'logger' 'plugins' 'weechat')
 
 mkdir -p "${WEECHAT_HOME}"
 for f in "${CONFFILES[@]}"; do ln -s -f -n "${CONFDIR}/${f}.conf" "${WEECHAT_HOME}/${f}.conf"; done
+
+
+BITLBEE_PREFIX_DEFAULT="${HOME}/build/bitlbee"
+BITLBEE_PREFIX="$(readlink -m "${1:-"${BITLBEE_PREFIX_DEFAULT}"}" )"
+
+BITLBEE_SRC_VER_DEFAULT="3.2.2"
+BITLBEE_SRC_VER="${2:-"${BITLBEE_SRC_VER_DEFAULT}"}"
+
+BITLBEE_BUILDDIR_ROOT_DEFAULT="/tmp"
+BITLBEE_BUILDDIR_ROOT="$(readlink -m "${3:-"${BITLBEE_BUILDDIR_ROOT_DEFAULT}"}" )"
 
 "${DIR}/install-bitlbee.sh" "${BITLBEE_SRC_VER}" "${BITLBEE_PREFIX}" "${BITLBEE_BUILDDIR_ROOT}"
 
